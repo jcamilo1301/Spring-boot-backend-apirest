c835593ca61d305ab471ceee588cad9eb727e6a7
juan camilo higuita velez <jcamilo1301@hotmail.com>
2018-08-01 17:41:04 -0500

    Hola mundo commit

---
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/PringBootBackendApirestApplication.java
@@ -0,0 +1,12 @@
+package com.bolsadeideas.springboot.backend.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PringBootBackendApirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PringBootBackendApirestApplication.class, args);
+	}
+}
