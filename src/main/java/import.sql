811ac024329ce23a4d0c7c596db943cbfe9e1ccd
juan camilo higuita velez <jcamilo1301@hotmail.com>
2018-08-02 15:42:48 -0500

    api

---
+++ src/main/java/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO clientes(nombre, apellido, email, create_at) values('Goku','Fernando','goku@email.com','2018-01-04');
+INSERT INTO clientes(nombre, apellido, email, create_at) values('Crillin','Javier','crillin@email.com','2018-01-05');
+INSERT INTO clientes(nombre, apellido, email, create_at) values('Gojan','Fernando','gojan@email.com','2018-01-06');
+INSERT INTO clientes(nombre, apellido, email, create_at) values('Vegeta','Restrepo','vegueta@email.com','2018-01-07');
+INSERT INTO clientes(nombre, apellido, email, create_at) values('numero17','Ramirez','n17@email.com','2018-01-08');
+INSERT INTO clientes(nombre, apellido, email, create_at) values('Picolo','Ramiro','picolo@email.com','2018-01-09');
